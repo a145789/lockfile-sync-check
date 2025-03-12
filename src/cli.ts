c00094372ebf3afd355fdf048be0f10341627d8a
a145789 <2091927351@qq.com>
2025-03-12 18:14:12 +0800

    fix: get argv logic

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,9 @@ import { installDependencies } from "./install"
 export const syncMessage = "\x1b[35mLockfile has been updated!\x1b[0m"
 
 const args = process.argv.slice(2)
-const packageManager = (args[0] as PackageManager | undefined) || "pnpm"
+const packageManager =
+  (args.filter((arg) => arg !== "--install")[0] as PackageManager) || "pnpm"
+
 const shouldInstall = args.includes("--install")
 
 const isNeedSync = checkLockfileSync(packageManager)
